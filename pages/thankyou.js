39ce3ca453d3d1028a3d14cc7b426298f004101a
toanalvin11 <81464449+toanalvin11@users.noreply.github.com>
2022-10-10 12:06:00 +0700

    [#13] thankyou-404-styles

---
+++ pages/thankyou.js
@@ -0,0 +1,53 @@
+import Head from "next/head";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import { Nav } from "react-bootstrap";
+import { useDispatch } from "react-redux";
+import Layout from "../components/Layout";
+import MenuBar from "../components/MenuBar";
+import { removeAll } from "../features/reducerCart";
+import style from "../styles/ThankYou.module.css"
+
+export default function ThankYou() {
+    const [message, setMessage] = useState("")
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        async function verify() {
+            const payId = localStorage.getItem("payId")
+            const token = localStorage.getItem("token")
+            const res = await fetch(`${process.env.NEXT_PUBLIC_API_ENDPOINT}/api/payment/verify?id=${payId}`, {
+                headers: {
+                    'Authorization': `Bearer ${token}`
+                },
+            })
+            const response = await res.json()
+            if (response.success === false) {
+                setMessage("Hi have a good day!")
+            }
+            else {
+                setMessage("Thanks for your purchase\nDownload games in your profile/purchased")
+                localStorage.removeItem('payId')
+                dispatch(removeAll())
+            }
+        }
+        verify()
+    }, [])//eslint-disable-line
+
+    return (
+        <div className={style.frame}>
+            <h1>{message}</h1>
+            <Link href={`/home/discover`} passHref><Nav.Link>Click here to continue buying</Nav.Link></Link>
+        </div>
+    )
+}
+
+ThankYou.getLayout = function getLayout(page) {
+    return (
+        <Layout>
+            <Head><title>Thank you</title></Head>
+            <MenuBar />
+            {page}
+        </Layout>
+    )
+}
